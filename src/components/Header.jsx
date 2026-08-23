2fa719b4dd3e4e0b14c592d366238c591050dcca
divrendatin-glitch <divrendatin31@gmail.com>
2026-08-23 17:02:26 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -548,12 +548,12 @@ export default function Header({ user, profile, onLogout, breadcrumbs = [], onNa
                                   <span 
                                     className="material-symbols-outlined filled-icon text-gray-600"
                                     style={{ 
-                                      fontSize: '64px',
+                                      fontSize: '80px',
                                       display: 'block',
                                       width: '100%',
                                       height: '100%',
                                       lineHeight: '1',
-                                      transform: 'scale(1.5)'
+                                      transform: 'scale(2)'
                                     }}
                                   >
                                     account_circle
